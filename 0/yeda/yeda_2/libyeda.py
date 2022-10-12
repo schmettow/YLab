a638a1cbf1cc44ae71076335234d3c5f8f7a3f96
schmettow <schmettow@web.de>
2022-10-12 21:18:56 +0200

    counting instances + docstrings added + update function can override sampling interval. Functions for resetting data + stiump for event handling

--- 0/yeda/yeda_2/libyeda.py
+++ 0/yeda/yeda_2/libyeda.py
@@ -5,25 +5,75 @@ from ulab import numpy as np
 from ulab import array as ary
 
 class Yeda:
+    n_instances = int(0)
 
-    def __init__(self, eda_pin = board.GP27_A1, interval = 0.1):
-        self.eda_pin = eda_pin
-        self.sensor = analogio.AnalogIn(eda_pin)
-        self.interval = interval
+    def __init__(self,  pin = board.GP27_A1, 
+                        update_interval = 0.1, 
+                        save_interval = 10):
+        """
+        Creates a new YEDA sensor object
+
+        :param eda_pin: IO port YEDA is connected to, default is Grove slot 
+        :param update_interval float: time between measures
+        :param save_interval float: time between pushing the collected data to a file
+        :return: None
+        :rtype: NoneType
+
+        """
+        # :raises ValueError: if the message_body exceeds 160 characters
+        # :raises TypeError: if the message_body is not a basestring
+
+        self.pin = pin
+        self.update_interval = update_interval
+        self.save_interval = save_interval
         self.time, self.last_time = (None, None)
         self.value, self.last_value = (None,None)
         self.data = np.empty(0,3)
-        self.events = np.empty(0,3)
+        self.event_data = np.empty(0,3)
+        self.sessionID = str(time.monotonic())
+        self.filename = "yeda_" + self.ID + ".csv"
+        self.signalID = "eda_" + str(Yeda.n_instances)
+        Yeda.n_instances = Yeda.n_instances + 1
+        
+    def connect(self):
+        """
+        Connects to the sensor port
+        """
+        self.sensor = analogio.AnalogIn(self.pin)
 
     def result(self):
         return (self.time, self.value, self.delta)
 
-    def update(self):
+    def read(self):
+        self.last_time = self.time
+        self.time = time.monotonic()
+
+        self.last_value = self.value
+        self.value = 65536 / self.sensor.value 
+        
+        self.delta = self.last_value - self.value
+        return self.result()
+
+    def update(self, interval = None, frequency = None):
+        """
+        Reads the sensor value
+
+        :param interval float: overrides update interval of object
+        :param frequency float: overrides interval as reads/s
+        :return: tuple (update, value), with the last value if no update has occured.
+        """
+
+        ## Catching the method arguments
+        if interval is None and frequency is None:
+            interval = self.interval
+        elif(isinstance(interval, float)):
+            interval = interval
+        elif(isinstance(frequency, float)):
+            interval = 1/frequency
+
         this_time = time.monotonic()
-        if self.interval > (this_time - self.last_time):
-            self.time = this_time
-            self.value = 65536 / self.sensor.value 
-            self.delta = self.last_value - self.value
+        if interval > (this_time - self.last_time):
+            self.read()
             return (True, self.result())
         else:
             return (False, self.result())
@@ -33,6 +83,21 @@ class Yeda:
 
     def record_event(self, label = "TOI", value = 1):
         this_time = time.monotonic()
-        np.append(self.data, (this_time, str(label), float(value)), 
+        np.append(self.event_data, (this_time, str(label), float(value)), 
                   axis=0)
+
+    def reset_data(self):
+        self.data = np.empty(0,3)
+
+    def save_data(self):
+        if int(time.monotonic()) % self.save_interval == 0: ## Modulo remains
+            np.savetxt(self.filename, 
+                self.data,
+                comments='#',
+                delimiter=',',
+                header='time,' + self.signalID + ',delta')
+            self.reset_data()
+    
+    def print(self):
+        print(self.value)
 
