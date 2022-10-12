e08854268d4d59400dc033322a5bd15d47d5aa45
schmettow <schmettow@web.de>
2022-10-12 21:16:41 +0200

    created setup function + removed some stuff

--- 0/yeda/yeda_2/code.py
+++ 0/yeda/yeda_2/code.py
@@ -1,51 +1,37 @@
+"""
+YEDA_2 improves on Yeda_1 by using libyeda (object-oriented)
+"""
+
+
 import board
 import time
 import pwmio
 import digitalio
 from analogio import AnalogIn
 from neopixel_write import neopixel_write
+from logger import logging as log
+from libyeda import Yeda
 
 DEBUG = True
-tick = 0.0001 ## 0.1 ms
-
-STATE = "Init"
-print(STATE)
-
-## Pico onboard LED
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-
-## Maker Pi onboard RGB led
-rgb_pin = board.GP28
-RGB = digitalio.DigitalInOut(rgb_pin)
-RGB.direction = digitalio.Direction.OUTPUT
-
-white = bytearray([10, 10, 10])
-red   = bytearray([0, 10, 0])
-green = bytearray([5, 0, 0])
-blue  = bytearray([0, 0, 3])
-
-## Maker Pi onboard button
-bt1_pin = board.GP20
-BT1 = digitalio.DigitalInOut(bt1_pin)
-BT1.switch_to_input(pull=digitalio.Pull.DOWN)
-
-## Grove GSR sensor connected to Maker Pi Grove 6
-## using onboard ADC
+update_interval = 0.01 ## 0.10 ms
+save_interval = 20 ## seconds
 yeda_pin = board.GP27_A1
-YEDA = AnalogIn(yeda_pin)
 
-print("YEDA1")
-
-def store_data():
-    pass
+print("YEDA 2")
 
 def main():
+
+    Yeda = Yeda(yeda_pin, update_interval)
+    try:
+        Yeda.connect()
+    except:
+        print("Error: Could not connect Yeda.")
+        exit()
 
     ## Initial state
     STATE = "Stop"
     this_BT1 = not BT1.value ## default state is closed
-    led.value = False
+    Led.value = False
     neopixel_write(RGB, white)
 
     ## Collecting data as list of tuples
@@ -62,15 +48,8 @@ def main():
     
     ## Fast while loop
     while True:
-        ## This is far from perfect, because
-        ##  the ticks also influence the responsiveness of the interface
-        ## Still, analogue sensors update continuously and we have to slow it down just a bit
-        time.sleep(tick)  
         now = time.monotonic()  ## this time
-
-
-
-        ############### Collecting button events ########
+        ############### Collecting button state changes ########
 
         ## Note that buttons are not like switches, as they bounce back.
         ## What matters is to discover state changes and create higher-level
@@ -81,7 +60,7 @@ def main():
         this_BT1 = not BT1.value # to press means to disrupt the circuit
         Event = this_BT1 != last_BT1
 
-        ################ Creating interaction events ############
+        ################ Creating button events ############
         ## Now the button event is classified further.
         ## By time stamps we can create more advanced interaction modes
                 
@@ -101,28 +80,58 @@ def main():
 
         ################ Interactive transitionals #############
 
-        
+        """
+        Note that RGB and Led get their values as updates. 
+        No need to create presentitionals. The order of the 
+        inner  conditions is completely arbitrary.
+        """
+
         if Event:
             if event_type == "BT1_short":
-                ## Stop --> Record
-                if STATE == "Record":
-                    STATE = "Pause"
-                    led.value = False
-                    neopixel_write(RGB, green)
-                elif STATE == "Stop" or STATE == "Pause":
-                    STATE = "Record"
-                    led.value = True
-                    neopixel_write(RGB, red)
                 ## Record --> Pause
+                if STATE == "Record":
+                    Led.value = False
+                    neopixel_write(RGB, green)
+                    STATE = "Pause"
+                ## Pause --> Record
+                elif STATE == "Pause":
+                    Led.value = True
+                    neopixel_write(RGB, red)
+                    STATE = "Record"
+                ## Stop --> Record
+                elif STATE == "Stop":
+                    Led.value = True
+                    neopixel_write(RGB, red)
+                    Yeda.reset_data()
+                    STATE = "Record"
+                ## --> STOP
             elif event_type == "BT1_long":
                 STATE = "Stop"
-                led.value = False
+                Led.value = False
                 neopixel_write(RGB, white)
-            # print(STATE) if DEBUG
+            
 
+        ################ Continuous processing #############
 
+        if STATE == "Record":
+            updated, value = Yeda.update()
+            if updated:
+                Yeda.print()
+                Yeda.record()
+                Yeda.save()
+        elif STATE == "Pause":
+            Yeda.update()
+            Yeda.print()
+        elif STATE == "Stop":
+            pass
+        else:
+            pass
 
-        ################ Continuous display #############
+        ################ Presentitionals #############
+        # Since we are using static displays (Leds), 
+        # we don't do anything here.
+        # A connected LCD screen would require 
+        # continuous updating
 
         if STATE == "Record":
             pass
@@ -133,12 +142,30 @@ def main():
         else:
             pass
 
-        ################ Data collection #####################
 
-        if STATE == "Record":
-            this_eda = 65536 / YEDA.value  ## take measure as Siemens
-            # OUT.append((now, this_eda)) ## careful with limited memory
-            print((this_eda,))  ## best viewed in a serial plotter
+
+def setup():
+    global Led, RGB, BT1, green ,red, white, blue
+
+    ## Pico onboard LED
+    Led = digitalio.DigitalInOut(board.led)
+    Led.direction = digitalio.Direction.OUTPUT
+
+    ## Maker Pi onboard RGB led
+    rgb_pin = board.GP28
+    RGB = digitalio.DigitalInOut(rgb_pin)
+    RGB.direction = digitalio.Direction.OUTPUT
+
+    white = bytearray([10, 10, 10])
+    red   = bytearray([0, 10, 0])
+    green = bytearray([5, 0, 0])
+    blue  = bytearray([0, 0, 3])
+
+    ## Maker Pi onboard button
+    bt1_pin = board.GP20
+    BT1 = digitalio.DigitalInOut(bt1_pin)
+    BT1.switch_to_input(pull=digitalio.Pull.DOWN)
+
 
 main()
 
