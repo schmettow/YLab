f900bbb4df270f75bf18452ccd5a28f4cdf6f533
schmettow <schmettow@web.de>
2022-10-13 20:23:49 +0200

    uses new YLab classes Button and RGB.

--- 0/yeda/yeda_2/code.py
+++ 0/yeda/yeda_2/code.py
@@ -5,123 +5,93 @@ YEDA_2 improves on Yeda_1 by using libyeda (object-oriented)
 
 import board
 import time
-import pwmio
-import digitalio
-from analogio import AnalogIn
-from neopixel_write import neopixel_write
-from logger import logging as log
-from libyeda import Yeda
 
-DEBUG = True
-update_interval = 0.01 ## 0.10 ms
-save_interval = 20 ## seconds
-yeda_pin = board.GP27_A1
+from libyeda import Yeda, this_moment
+from libylab import Button, RGB
+
+HZ = 10 ## sampling frequency in Hz
+UPDATE = 1/HZ
+PIN = board.GP27_A1
+SAVE = 1
+PRINT = False
+
 
 print("YEDA 2")
-
 def main():
 
-    Yeda = Yeda(yeda_pin, update_interval)
+    Yeda_0 = Yeda( pin = PIN,
+                        update_interval = UPDATE,
+                        save_interval = SAVE )
     try:
-        Yeda.connect()
+        Yeda_0.connect()
     except:
-        print("Error: Could not connect Yeda.")
+        print("Error: Could not connect Yeda_0.")
         exit()
-
-    ## Initial state
+    
+    BT2 = Button(pin = board.GP21)
+    BT2.connect()
+    
+    Rgb = RGB()
+    Rgb.connect()
+    Rgb.white()
+    
     STATE = "Stop"
-    this_BT1 = not BT1.value ## default state is closed
-    Led.value = False
-    neopixel_write(RGB, white)
-
-    ## Collecting data as list of tuples
-    OUT = []
-
-    ## Creating initial button Events
     print(STATE)
     
-    ## Preparing event handling
-    ## Mark event on button state change
-    event_time = 0
-    event_type = ""
-
+    
+    
     
     ## Fast while loop
     while True:
-        now = time.monotonic()  ## this time
-        ############### Collecting button state changes ########
-
-        ## Note that buttons are not like switches, as they bounce back.
-        ## What matters is to discover state changes and create higher-level
-        ## events. 
-        ## What matters first is to discover when the button changes its state
-        
-        last_BT1 = this_BT1
-        this_BT1 = not BT1.value # to press means to disrupt the circuit
-        Event = this_BT1 != last_BT1
-
-        ################ Creating button events ############
-        ## Now the button event is classified further.
-        ## By time stamps we can create more advanced interaction modes
+        now = this_moment()  ## this time
                 
-        if Event:
-            ## remembering the last event time
-            last_event_time = event_time
-            event_time = now
-            
-            if not this_BT1: ## released after short or long press
-                if (event_time - last_event_time) < 1: ## short release
-                    event_type = "BT1_short"
-                else:
-                    event_type = "BT1_long" ## long release
-            else:
-                event_type = "BT1_press"
-            # print(event_type)
-
         ################ Interactive transitionals #############
 
-        """
-        Note that RGB and Led get their values as updates. 
-        No need to create presentitionals. The order of the 
-        inner  conditions is completely arbitrary.
-        """
 
-        if Event:
-            if event_type == "BT1_short":
-                ## Record --> Pause
+        # Note that RGB is static. You write a value and it stays. 
+        # No need to create presentitionals. The order of the 
+        # inner  conditions is completely arbitrary. The order is entirely
+        # determined by transitions.
+        
+        if BT2.sample(): # ha sthe state changed?
+            if BT2.update_event(): # has there be a new event?
+                if BT2.event == "short":
+                    ## Record --> Pause
+                    if STATE == "Record":
+                        STATE = "Pause"
+                    ## Pause --> Record
+                    elif STATE == "Pause":
+                        STATE = "Record"
+                    ## Stop --> Record
+                    elif STATE == "Stop":
+                        Yeda_0.reset_data()
+                        STATE = "Record"
+                    ## --> STOP
+                elif BT2.event == "long":
+                    STATE = "Stop"
+                    #Yeda_0.save_data()
+                
+                ## Updating the static displays ##
                 if STATE == "Record":
-                    Led.value = False
-                    neopixel_write(RGB, green)
-                    STATE = "Pause"
-                ## Pause --> Record
+                    Rgb.red()
                 elif STATE == "Pause":
-                    Led.value = True
-                    neopixel_write(RGB, red)
-                    STATE = "Record"
-                ## Stop --> Record
+                    Rgb.green()
                 elif STATE == "Stop":
-                    Led.value = True
-                    neopixel_write(RGB, red)
-                    Yeda.reset_data()
-                    STATE = "Record"
-                ## --> STOP
-            elif event_type == "BT1_long":
-                STATE = "Stop"
-                Led.value = False
-                neopixel_write(RGB, white)
+                    Rgb.white
+                
+            print(STATE)
             
 
         ################ Continuous processing #############
 
         if STATE == "Record":
-            updated, value = Yeda.update()
-            if updated:
-                Yeda.print()
-                Yeda.record()
-                Yeda.save()
+            if Yeda_0.sample():
+                #Yeda_0.print()
+                Yeda_0.record()
+                #Yeda_0.save_data()
         elif STATE == "Pause":
-            Yeda.update()
-            Yeda.print()
+            Yeda_0.sample()
+            #Yeda_0.print()
         elif STATE == "Stop":
             pass
         else:
@@ -141,30 +111,6 @@ def main():
             pass
         else:
             pass
-
-
-
-def setup():
-    global Led, RGB, BT1, green ,red, white, blue
-
-    ## Pico onboard LED
-    Led = digitalio.DigitalInOut(board.led)
-    Led.direction = digitalio.Direction.OUTPUT
-
-    ## Maker Pi onboard RGB led
-    rgb_pin = board.GP28
-    RGB = digitalio.DigitalInOut(rgb_pin)
-    RGB.direction = digitalio.Direction.OUTPUT
-
-    white = bytearray([10, 10, 10])
-    red   = bytearray([0, 10, 0])
-    green = bytearray([5, 0, 0])
-    blue  = bytearray([0, 0, 3])
-
-    ## Maker Pi onboard button
-    bt1_pin = board.GP20
-    BT1 = digitalio.DigitalInOut(bt1_pin)
-    BT1.switch_to_input(pull=digitalio.Pull.DOWN)
 
 
 main()
