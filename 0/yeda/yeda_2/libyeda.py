6c88e3f37f5a4436a28555596ef0bae92dabf081
schmettow <schmettow@web.de>
2022-10-14 23:16:55 +0200

    removed default pin + signalID is called ID now + this_moment() + new storage functions

--- 0/yeda/yeda_2/libyeda.py
+++ 0/yeda/yeda_2/libyeda.py
@@ -1,15 +1,21 @@
 import board
+import os
 import time
 import analogio
+import storage
+import busio
+import sdcardio
 from ulab import numpy as np
-from ulab import array as ary
+
+def this_moment():
+    return time.time()
 
 class Yeda:
     n_instances = int(0)
 
-    def __init__(self,  pin = board.GP27_A1, 
-                        update_interval = 0.1, 
-                        save_interval = 10):
+    def __init__(self,  pin,
+                        update_interval, 
+                        save_interval):
         """
         Creates a new YEDA sensor object
 
@@ -20,20 +26,18 @@ class Yeda:
         :rtype: NoneType
 
         """
-        # :raises ValueError: if the message_body exceeds 160 characters
-        # :raises TypeError: if the message_body is not a basestring
 
+        self.ID = "eda_" + str(Yeda.n_instances) ## auto-counting multiple Yeda devices
         self.pin = pin
         self.update_interval = update_interval
         self.save_interval = save_interval
         self.time, self.last_time = (None, None)
         self.value, self.last_value = (None,None)
-        self.data = np.empty(0,3)
-        self.event_data = np.empty(0,3)
-        self.sessionID = str(time.monotonic())
-        self.filename = "yeda_" + self.ID + ".csv"
-        self.signalID = "eda_" + str(Yeda.n_instances)
-        Yeda.n_instances = Yeda.n_instances + 1
+        self.data = np.empty([0,2])
+        self.sessionID = str(this_moment())
+        self.filename = "yeda_" + self.ID + ".npy"     ## CSV file to collect data
+        self.last_saved = None
+        Yeda.n_instances = Yeda.n_instances + 1        ## auto-counting multiple Yeda devices
         
     def connect(self):
         """
@@ -42,62 +46,98 @@ class Yeda:
         self.sensor = analogio.AnalogIn(self.pin)
 
     def result(self):
-        return (self.time, self.value, self.delta)
+        return (self.time, self.value)
 
     def read(self):
-        self.last_time = self.time
-        self.time = time.monotonic()
-
+        now = this_moment()        
         self.last_value = self.value
-        self.value = 65536 / self.sensor.value 
-        
-        self.delta = self.last_value - self.value
+        self.value = 65536 / self.sensor.value
+        if self.time is None: ## the first read
+            self.last_time = this_moment
         return self.result()
 
-    def update(self, interval = None, frequency = None):
+    def sample(self, interval = None, frequency = None):
         """
         Reads the sensor value
 
         :param interval float: overrides update interval of object
         :param frequency float: overrides interval as reads/s
-        :return: tuple (update, value), with the last value if no update has occured.
+        :return: logical value whether an update has been performed
         """
 
         ## Catching the method arguments
         if interval is None and frequency is None:
-            interval = self.interval
+            interval = self.update_interval
         elif(isinstance(interval, float)):
             interval = interval
         elif(isinstance(frequency, float)):
             interval = 1/frequency
-
-        this_time = time.monotonic()
-        if interval > (this_time - self.last_time):
-            self.read()
-            return (True, self.result())
         else:
-            return (False, self.result())
+            print("sample(): arguments interval and frequency must either be omitted, or used one at a time.")
+
+        now = this_moment()
+        if self.time is None: ## first read
+            self.read()
+        elif interval > (this_moment - self.last_time):
+            self.read()
+            return True
+        else:
+            return False
 
     def record(self):
         np.append(self.data, self.result(), axis=0)
 
-    def record_event(self, label = "TOI", value = 1):
-        this_time = time.monotonic()
-        np.append(self.event_data, (this_time, str(label), float(value)), 
+    def record_event(self, label = "MOI", value = 1):
+        now = this_moment()
+        np.append(self.event_data, (this_moment, str(label), float(value)), 
                   axis=0)
 
     def reset_data(self):
-        self.data = np.empty(0,3)
+        self.data = np.empty([0,2])
+
+    def init_storage():
+        cs = board.GP15
+        spi = busio.SPI(board.GP10, MOSI=board.GP11, MISO=board.GP12)
+        sd = sdcardio.SDCard(spi, cs)
+        vfs = storage.VfsFat(sd)
+        storage.mount(vfs, '/sd')
+        
+    def release_storage():
+        storage.umount(vfs)
+        spi.deinit()
+        sd.deinit()
+
 
     def save_data(self):
-        if int(time.monotonic()) % self.save_interval == 0: ## Modulo remains
-            np.savetxt(self.filename, 
-                self.data,
-                comments='#',
-                delimiter=',',
-                header='time,' + self.signalID + ',delta')
-            self.reset_data()
+        if self.last_saved is None:
+            mode = "w"
+        else:
+            mode = "a"
+        Yeda.init_storage()
+        path = os.path.join("/sd", self.filename)
+        Yeda.release_storage()
+        
+#         with open(self.filename, mode) as file:
+#             for row in self.data:
+#                 file.write(row[0], "," ,row[1],"\n")
+#        file.close()
+    
+    def move_data(self):
+        now = time_monotonic()
+        if self.last_saved is None: # first time
+            self.save()
+            self.last_saved = this_moment
+        time_since = this_moment - self.last_saved
+        if (this_moment() - self.last_saved) >= self.save_interval:
+            
+                
+                self.reset_data()
     
     def print(self):
-        print(self.value)
+        # out = self.ID +":"+ self.value
+        out = self.value
+        print(out)
+        
+
+
 
