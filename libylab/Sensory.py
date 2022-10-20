de87b4fabf1e23a397c4b4ee5ebd333382fdbc52
schmettow <schmettow@web.de>
2022-10-20 20:39:35 +0200

    record is now buffer() + methods data_dim() and n_obs() + demo() is now classmethod

--- libylab/Sensory.py
+++ libylab/Sensory.py
@@ -14,14 +14,12 @@ class Sensor:
     pins = None
 
     def __init__(self,  pins = None,
-                        sample_interval = None, 
-                        save_interval = None):
+                        sample_interval = None):
         """
         Creates a new YedaADS sensor object
 
         :param eda_pin: IO port YedaADS is connected to, default is Grove slot 
         :param sample_interval float: time between measures
-        :param save_interval float: time between pushing the collected data to a file
         :return: None
         :rtype: NoneType
 
@@ -30,8 +28,6 @@ class Sensor:
             self.pins = pins
         if not sample_interval is None:
             self.sample_interval = sample_interval
-        if not save_interval is None:
-            self.save_interval = save_interval
         self.sensor_class = self.__class__.__name__
         self.ID = self.sensor_class + str(Sensor.n_instances) ## auto-counting multiple devices
         self.time = this_moment()
@@ -48,8 +44,7 @@ class Sensor:
         except:
             print("Failed first read.")
             return False
-
-
+    
     def read(self):
         return self.time % 42
 
@@ -86,34 +81,95 @@ class Sensor:
         :rtype: tuple
 
         """
-        out = [[self.time], [self.ID], [self.value]]
+        if isinstance(self.time, (list, tuple)):
+            out = [self.time, self.ID, self.value]
+        else:
+            out = [[self.time], [self.ID], [self.value]]
         return out
 
-    def record(self):
+    def buffer(self):
         result = self.result()
+        n_result = len(result[0])
         for col in range(0, len(result)):
-            self.data[col].append(result[col][0])
-        return True
-        
+            self.data[col].append(result[col][0])  ## <<- bad fix
+        return n_result
+    
+    def record(self):
+        return self.buffer()
+   
+    def data_dim(self):
+        return (len(self.data[0]), len(self.data))
+   
+    def n_obs(self):
+        return len(self.data[0])
+    
     def print(self):
         out = self.ID +":"+ str(self.value)
         print(out)
 
-
-    def demo():
-        sensor = Sensor()
+    @classmethod
+    def demo(cls):
+        sensor = cls()
         sensor.connect()
         sensor.sample_interval = 0.2
         while True:
             if sensor.sample():
                 sensor.print()
-    
+                print(sensor.result())
     def reset_data(self):
         self.data = [[], [], []]
         return True
 
+
+
+
+class Sensory(Sensor):
+    """
+    A sensor array
+    
+    """
+    
+    
+    
+    def __init__(self, sensor_array):
+        self.sensors = sensor_array
+        self.ID = []
+        self.time = []
+        self.value = []
+        self.data = [[],[],[]]
+    
+    def connect(self):
+        success = True
+        for sensor in self.sensors:
+            if not sensor.connect():
+                success = false
+        return success
+        
+    def sample(self):
+        self.ID = []
+        self.time = []
+        self.value = []
+        new_sample = 0
+        for sensor in self.sensors:
+            if sensor.sample():
+                self.ID.append(sensor.ID)
+                self.time.append(sensor.time)
+                self.value.append(sensor.value)
+                new_sample += 1
+        return new_sample
+    
+         
+    def print(self):
+        out_string = ""
+        for sensor in self.sensors:
+            out_string = out_string + sensor.ID + ":" + str(sensor.value) + " "
+        print(out_string)
+        return True
+    
+    
+
+
 class AnalogSensor(Sensor):
-    n_instances = int(0)
     pins = board.GP27
     bit_width = 11
     reciprocal = False
@@ -136,20 +192,6 @@ class AnalogSensor(Sensor):
             return value
         else:
             return 1/value
-
-
-
-class Yeda(AnalogSensor):
-    pins = board.GP27
-    reciprocal = True
-
-    def demo():
-        sensor = Yeda()
-        sensor.connect()
-        sensor.sample_interval = 0.2
-        while True:
-            if sensor.sample():
-                sensor.print()
 
 
 
@@ -185,47 +227,12 @@ class SensorADS(AnalogSensor):
         self.sensor = AnalogIn(SensorADS.ads, ADS.P0)
         return Sensor.connect(self)
 
+
+class Yeda(AnalogSensor):
+    pins = board.GP27
+    reciprocal = True
+
 class Yema(SensorADS):
     reciprocal = True
-    def demo():
-        sensor = Yema()
-        sensor.connect()
-        sensor.sample_interval = 0.2
-        while True:
-            if sensor.sample():
-                sensor.print()
 
-class Sensory(Sensor):
-    
-    def __init__(self, sensor_array):
-        self.sensors = sensor_array
-    
-    def connect(self):
-        success = True
-        for sensor in self.sensors:
-            if not sensor.connect():
-                success = false
-        return success
-        
-    def record(self):
-        new_sample = False
-        for sensor in self.sensors:
-            if sensor.sample():
-                new_sample = True
-                
-        return new_sample
-    
-    def record(self):
-        new_sample = False
-        for sensor in self.sensors:
-            if sensor.sample():
-                new_sample = True
-        return new_sample
-    
-    def print(self):
-        out_string = ""
-        for sensor in self.sensors:
-            out_string = out_string + sensor.ID + ":" + str(sensor.value) + " "
-        print(out_string)
-        return True
 
